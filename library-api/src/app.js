0fc5141d5ffb190e97fc4ff6bd66372eddc994af
JeinLo <evgbaranovae11@gmail.com>
2025-10-21 10:22:09 +0300

    вариант 1

--- library-api/src/app.js
+++ library-api/src/app.js
@@ -11,6 +11,14 @@ const routes = require('./routes');
 dotenv.config();
 const { PORT = 3005, API_URL = 'http://127.0.0.1', MONGO_URL = 'mongodb://localhost:27017/mydb' } = process.env;
 
+mongoose.connect(MONGO_URL)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server started at ${API_URL}:${PORT}`);
+    });
+  })
+  .catch(err => console.error(`Failed to connect to MongoDB: ${err.message}`));
+
 const app = express();
 
 app.use(bodyParser.json());
@@ -25,10 +33,3 @@ app.use((req, res) => {
 
 app.use(errorHandler);
 
-mongoose.connect(MONGO_URL)
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server started at ${API_URL}:${PORT}`);
-    });
-  })
-  .catch(err => console.error(`Failed to connect to MongoDB: ${err.message}`));
